6beb5c90e1758249d99b99ae7029b558aad1c5d0
AfagMamedova-JU23GBG <afag.mamedova@iths.se>
2024-11-28 15:12:46 +0100

    uppdaterade test classen

--- src/test/java/TicTacToeModelTest.java
+++ src/test/java/TicTacToeModelTest.java
@@ -1,4 +1,52 @@
 import static org.junit.jupiter.api.Assertions.*;
-class TicTacToeModelTest {
-  
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeModelTest {
+
+    @Test
+    void checkWin() {
+        TicTacToeModel model = new TicTacToeModel();
+        model.makeMove(0, 0);
+        model.makeMove(1, 0);
+        model.makeMove(0, 1);
+        model.makeMove(1, 1);
+        model.makeMove(0, 2);
+        assertEquals('X', model.checkWin());
+    }
+
+    @Test
+    void checkWinCol() {
+        TicTacToeModel model = new TicTacToeModel();
+        model.makeMove(0, 0);
+        model.makeMove(0, 1);
+        model.makeMove(1, 0);
+        model.makeMove(1, 1);
+        model.makeMove(2, 0);
+        assertEquals('X', model.checkWin());
+    }
+
+    @Test
+    void checkDraw() {
+        TicTacToeModel model = new TicTacToeModel();
+        model.makeMove(0, 0);
+        model.computerMove(); // O
+        model.makeMove(0, 1);
+        model.computerMove(); // O
+        model.makeMove(0, 2);
+        model.computerMove(); // O
+        model.makeMove(1, 0);
+        model.computerMove(); // O
+        model.makeMove(1, 1);
+        model.computerMove(); // O
+        model.makeMove(1, 2);
+        model.computerMove(); // O
+        model.makeMove(2, 0);
+        model.computerMove(); // O
+        model.makeMove(2, 1);
+        model.computerMove(); // O
+        model.makeMove(2, 2);
+
+        assertTrue(model.isBoardFull());
+        assertEquals(' ', model.checkWin());
+    }
 }
